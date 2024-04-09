00e815593ed0e29d0c886d26870694c5fb90bc20
AliReza Doostmohammadi <alireza.dm80@gmail.com>
2024-04-10 01:19:04 +0330

    Add static and media paths in settings.py

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #My Apps
+    'product.apps.ProductConfig',
+    'cart.apps.CartConfig',
+    'order.apps.OrderConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -121,3 +127,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My Configurations
+
+STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
