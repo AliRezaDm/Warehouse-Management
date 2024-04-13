bc85ab2c70c4e05fbe66263a430754191344440c
AliReza Doostmohammadi <alireza.dm80@gmail.com>
2024-04-13 09:55:51 +0330

    Add Admin for Variant, Supply, Category

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Variant, Supply, Category
 
-# Register your models here.
+
+@admin.register(Supply)
+class SupplyAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'image_tag','title', 'status')
+    list_filter = (['id', 'status'])
+    search_fields = ('id', 'title')
+    ordering = ['id']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'title')
+    list_filter = (['id', 'title'])
+    search_fields = ('id', 'title')
+    ordering = ['id']
+
+@admin.register(Variant)
+class VariantAdmin(admin.ModelAdmin):
+    
+    list_display=('supply', 'color', 'size', 'count')
