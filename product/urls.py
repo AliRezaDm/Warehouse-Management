1454c5092e0959ddac3c3b4c827101c328ee561d
AliReza Doostmohammadi <alireza.dm80@gmail.com>
2024-04-13 11:45:09 +0330

    Update product urls.py

--- product/urls.py
+++ product/urls.py
@@ -4,5 +4,20 @@ from . import views
 app_name ='product'
 urlpatterns = [
     path ('', views.VariantListView.as_view(), name='variant_list'),
-    path ('detail/<id>', views.VariantDetailView.as_view(), name='variant_detail')
+    path ('detail/<id>', views.VariantDetailView.as_view(), name='variant_detail'),
+
+    # Category Urls  
+    path('category/', views.CategoryList.as_view(), name='category_list'),
+    path('create_category/', views.CategoryCreateView.as_view(), name='category_create' ), 
+
+    # Color Urls
+    path('create_color/', views.ColorCreateView.as_view(), name='color_create' ),
+    path('color_list/', views.ColorListView.as_view(), name='color_list' ),
+
+    # Size Urls
+    path('create_size/', views.SizeCreateView.as_view(), name='size_create' ), 
+    path('size_list/', views.SizeListView.as_view(), name='size_list' ), 
+
+    # Search Url
+    path ('search/', views.search_view, name='search'),
 ]
