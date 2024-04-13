856df07286a1e058db2a7871b493e84c1736d723
AliReza Doostmohammadi <alireza.dm80@gmail.com>
2024-04-14 00:06:56 +0330

    Add cart model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from product.models import Variant
+from accounts.models import BaseUser
+from django.utils import timezone
 
-# Create your models here.
+class Cart(models.Model):
+
+    user = models.ForeignKey(BaseUser, verbose_name=' نام کاربر', on_delete=models.CASCADE, related_name='cart_user')
+    create_at = models.DateTimeField(default=timezone.now())
+
+class CartItem(models.Model):
+
+    cart = models.ForeignKey(Cart, verbose_name=' لیست', on_delete=models.CASCADE, related_name='cart_items')
+    variant =  models.ForeignKey(Variant, verbose_name='نام محصول', on_delete=models.CASCADE, related_name='cart_supply', unique=True)
+    quantity = models.PositiveIntegerField(verbose_name='تعداد محصول')
+    
+
+
