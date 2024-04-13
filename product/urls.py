51b535b1079608e0e9d1a1d8b4fc0d8836f72c87
AliReza Doostmohammadi <alireza.dm80@gmail.com>
2024-04-13 09:50:00 +0330

    Add view and url for variant

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from . import views
 
-
-
+app_name ='product'
 urlpatterns = [
-    
+    path ('', views.VariantListView.as_view(), name='variant_list'),
+    path ('detail/<id>', views.VariantDetailView.as_view(), name='variant_detail')
 ]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from typing import Any
+from django.db.models.base import Model as Model
+from django.db.models.query import QuerySet
+from django.views.generic import ListView, DetailView 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Variant
 
-# Create your views here.
+
+class VariantListView(ListView):
+
+    model = Variant
+    template_name = 'product/variant_list.html'
+
+class VariantDetailView(DetailView):
+
+    model = Variant
+    template_name = 'product/variant_detail.html'
+
+    def get_object(self):
+        self.kwargs.get('id')
+        return get_object_or_404(Variant.objects.all(), pk=id)
