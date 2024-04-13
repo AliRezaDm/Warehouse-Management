4ea0e4349fbdd1ecfe4505ed242a5dcae373e9d8
AliReza Doostmohammadi <alireza.dm80@gmail.com>
2024-04-13 11:33:26 +0330

    Add Color and Size views

--- product/views.py
+++ product/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.db.models.base import Model as Model
-
 from django.views.generic import ListView, DetailView 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Variant, Supply, Category
+
+from .models import Variant, Supply, Category, Color, Size
 
 
 class VariantListView(ListView):
@@ -21,9 +21,8 @@ class VariantDetailView(DetailView):
         self.kwargs.get('id')
         return get_object_or_404(Variant.objects.all(), pk=id)
 
-
-
-
+#---------------------------------------------------------------------------------
+# Category view -> CreateView, UpdateView, DeleteVeiw, ListView
 class CategoryList(ListView):
 
     model = Category
@@ -46,3 +45,50 @@ class CategoryDeleteView(DeleteView):
 
     model = Category
     success_url = reverse_lazy('product:category_list')
+
+#---------------------------------------------------------------------------------
+# Size view -> CreateView, UpdateView, ListView
+class SizeCreateView(CreateView):
+
+    model = Size
+    fields = ['name']
+    template_name = "product/add_size_form.html"
+
+class SizeUpdateView(UpdateView):
+
+    model = Size
+    fields = ['name']
+    template_name = "product/add_size_form.html"
+
+class SizeListView(ListView):
+    
+    model = Size
+    template_name = 'product/size_list.html'
+    queryset = Size.objects.all()
+    
+#---------------------------------------------------------------------------------
+# Color view -> CreateView, UpdateView, ListView
+class ColorCreateView(CreateView):
+
+    model = Color
+    fields = ['name']
+    template_name = "product/add_color_form.html"
+
+class ColorUpdateView(UpdateView):
+
+    model = Color
+    fields = ['name']
+    template_name = "product/add_color_form.html"
+
+class ColorListView(ListView):
+    
+    model = Color
+    template_name = 'product/color_list.html'
+    queryset = Color.objects.all()
+
+
+
+
+
+
+
