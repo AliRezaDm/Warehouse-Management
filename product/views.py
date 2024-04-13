55344fb6845450eec7cb46113cf69ce60392ba79
AliReza Doostmohammadi <alireza.dm80@gmail.com>
2024-04-13 11:30:39 +0330

    Add Category views

--- product/views.py
+++ product/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import get_object_or_404
-from typing import Any
+from django.urls import reverse_lazy
 from django.db.models.base import Model as Model
-from django.db.models.query import QuerySet
+
 from django.views.generic import ListView, DetailView 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Variant
+from .models import Variant, Supply, Category
 
 
 class VariantListView(ListView):
@@ -20,3 +20,29 @@ class VariantDetailView(DetailView):
     def get_object(self):
         self.kwargs.get('id')
         return get_object_or_404(Variant.objects.all(), pk=id)
+
+
+
+
+class CategoryList(ListView):
+
+    model = Category
+    queryset = Category.objects.filter(status=True)
+    template_name = "product/category_list"
+
+class CategoryCreateView(CreateView):
+
+    model = Category
+    fields = ['parent', 'title', 'status']
+    template_name = "product/add_category_form.html"
+
+class CategoryUpdateView(UpdateView):
+
+    model = Category
+    fields = ['parent', 'title', 'status']
+    template_name = "product/add_category_form.html"
+
+class CategoryDeleteView(DeleteView):
+
+    model = Category
+    success_url = reverse_lazy('product:category_list')
