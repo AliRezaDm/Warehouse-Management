1e4e7b1c52c9887094a42916b1de7ef1b45d4602
AliReza Doostmohammadi <alireza.dm80@gmail.com>
2024-04-22 11:32:09 +0330

    Add views for order

--- order/views.py
+++ order/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from .models import Order, OrderItem
+from cart.models import Cart
+from product.models import Variant
 
-# Create your views here.
+@login_required()
+def order_detail(request, order_id):
+    order = Order.objects.get(id=order_id)
+    context = {'order':order}
+    return render(request, 'order/order.html', context)
+
+
+@require_POST
+@login_required()
+def order_create(request):
+    order = Order.objects.create(user_id=request.user.id)
+    
+    cart = Cart(request)
+    for item in cart:
+        OrderItem.objects.create(order_id=order.id, variant=item['variant'], price=item['price'], quantity=item['quantity'])
+        variant = Variant.objects.filter(id= item['variant'].id)
+        for data in variant:
+            data.count -= item['quantity']
+            data.save()
+    
+    Cart.objects.filter(user_id=request.user.id).delete()
+    
+    return redirect('order-detail', order.id)
