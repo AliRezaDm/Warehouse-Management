fc98edd06b33d5c6f57763e52dc6e8f597f58ca4
AliReza Doostmohammadi <alireza.dm80@gmail.com>
2024-04-23 12:08:26 +0330

    Add admin for cart

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    list_display = ['cart', 'variant', 'size', 'color', 'price', 'quantity', 'total_price']
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['user', 'create_at', 'get_total_price']
+    inlines = [CartItemInline]
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['cart', 'variant', 'size', 'color', 'price', 'quantity', 'total_price']
+
