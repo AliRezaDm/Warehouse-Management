a65ee8e0a8c91e74a1998473a519d3bed5ee18e1
AliReza Doostmohammadi <alireza.dm80@gmail.com>
2024-04-23 12:05:47 +0330

    Update cart/models.py

--- cart/models.py
+++ cart/models.py
@@ -8,11 +8,41 @@ class Cart(models.Model):
     user = models.ForeignKey(BaseUser, verbose_name=' نام کاربر', on_delete=models.CASCADE, related_name='cart_user')
     create_at = models.DateTimeField(default=timezone.now())
 
+    def __str__(self):
+        return f' کاربر:{self.user.username} '
+
+    @property
+    def get_total_price(self):
+        return sum(i.total_price() for i in self.cart_items.all())
+
+    class Meta:
+    
+        verbose_name = 'سبد خرید'
+        verbose_name_plural = ' سبد خرید'
+
 class CartItem(models.Model):
 
-    cart = models.ForeignKey(Cart, verbose_name=' لیست', on_delete=models.CASCADE, related_name='cart_items')
+    cart = models.ForeignKey(Cart, verbose_name=' سبد خرید', on_delete=models.CASCADE, related_name='cart_items')
     variant =  models.ForeignKey(Variant, verbose_name='نام محصول', on_delete=models.CASCADE, related_name='cart_supply', unique=True)
     quantity = models.PositiveIntegerField(verbose_name='تعداد محصول')
+
+    def __str__(self):
+        return f' کاربر:{self.cart.user.username} '
     
 
+    def size(self):
+        return self.variant.size.name
+    
 
+    def color(self):
+        return self.variant.color.name
+    
+    def price(self):
+        return self.variant.price
+
+    def total_price(self):
+        return self.variant.price * self.quantity
+
+    class Meta:
+        verbose_name = 'اقلام سبد خرید'
+        verbose_name_plural = ' اقلام سبد خرید'
