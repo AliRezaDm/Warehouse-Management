eb0f3f11f603b2eaba743244eabce85f7e8433a5
AliReza Doostmohammadi <alireza.dm80@gmail.com>
2024-04-23 12:28:37 +0330

    Fix order

--- order/views.py
+++ order/views.py
@@ -2,29 +2,38 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from .models import Order, OrderItem
-from cart.models import Cart
+from cart.models import Cart, CartItem
 from product.models import Variant
 
 @login_required()
-def order_detail(request, order_id):
-    order = Order.objects.get(id=order_id)
-    context = {'order':order}
-    return render(request, 'order/order.html', context)
+def order_detail(request):
+    orders = Order.objects.prefetch_related('order_items').all()
+    
+    context = {
+        'orders':orders,
+    }
+    
+    return render(request, 'order/order_list.html', context)
 
 
 @require_POST
 @login_required()
 def order_create(request):
-    order = Order.objects.create(user_id=request.user.id)
     
-    cart = Cart(request)
-    for item in cart:
-        OrderItem.objects.create(order_id=order.id, variant=item['variant'], price=item['price'], quantity=item['quantity'])
-        variant = Variant.objects.filter(id= item['variant'].id)
-        for data in variant:
-            data.count -= item['quantity']
-            data.save()
+    customer_name = request.POST.get('customer_name')
+    customer_phone = request.POST.get('customer_phone')
+    
+    order = Order.objects.create(user_id=request.user.id, customer_name=customer_name, customer_phone_number=customer_phone, is_paid=True)
+    
+    cart = Cart.objects.get(user_id=request.user.id)
+    
+    cart_items = CartItem.objects.filter(cart_id = cart)
+    
+    for item in cart_items:
+        OrderItem.objects.create(order_id=order.id, variant=item.variant, price=item.variant.price, quantity=item.quantity)
+        variant = Variant.objects.get(id= item.variant.id)
+        variant.inventory -= item.quantity
     
     Cart.objects.filter(user_id=request.user.id).delete()
     
-    return redirect('order-detail', order.id)
+    return redirect('order:order_detail')
